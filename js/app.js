fb08ad002176495901f43ea9b31b08ce832bde16
abrheem16 <abrheem16@gmail.com>
2019-08-10 22:27:20 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -137,8 +137,8 @@ app.request.json(list, function (data) {
         $("#btns").slice(0, 10).show(); // select the first ten
         $("#load").click(function(e){ // click event for load more
             e.preventDefault();
-            $("div:hidden").slice(0, 10).show(); // select next 10 hidden divs and show them
-            if($("div:hidden").length == 0){ // check if any hidden divs still exist
+            $("#btns:hidden").slice(0, 10).show(); // select next 10 hidden divs and show them
+            if($("#btns:hidden").length == 0){ // check if any hidden divs still exist
                 alert("No more divs"); // alert if there are none left
             }
         });
