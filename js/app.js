02e8c66cd7e5660387478e947a619bbaa5c849fb
abrheem16 <abrheem16@gmail.com>
2019-08-21 20:22:15 +0300

    Update

--- js/app.js
+++ js/app.js
@@ -83,7 +83,7 @@ function createitem(img,name,title,id,state,starts) {
   
   infos.className = "";
   
-  infos.href = id;
+  infos.href = "/list/;
   //Div
   var div1 = document.createElement("div");
   div1.style = 'style="height: 160;"';
